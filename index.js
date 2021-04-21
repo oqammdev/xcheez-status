af081531bf2580f435a47c17605bae64868969a7
XcHeeZ <akhgakamirb@gmail.com>
2021-04-22 01:22:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const client = new Discord.Client()
 
 client.on("ready", async()=>{
    console.log("ready")
+})
+client.on("message", async(message)=>{
    
    const guild = client.guilds.cache.get("792520918312288297")
 
@@ -14,6 +16,12 @@ const embed = new Discord.MessageEmbed()
 .setColor("BLUE")
 .setDescription("Bots Status")
 client.users.cache.filter(u => u.bot === true).forEach(bot => embed.addField(bot.username, bot.presence.status === "online"?"**🟢ONLINE**":"**🔴OFFLINE**"))
+   
+   let msg = await message.channel.send(embed)
+   
+   setInterval(function(){
+      msg.edit(embed)
+   }, 5000)
 
 
 })
