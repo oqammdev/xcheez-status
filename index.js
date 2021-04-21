42fe2a8b62a730f5865a3b4159daf89cb3e4144e
XcHeeZ <akhgakamirb@gmail.com>
2021-04-22 01:20:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js")
-const webhook = new Discord.WebhookClient(process.env.id, process.env.secret)
 const client = new Discord.Client()
 
 client.on("ready", async()=>{
@@ -16,11 +15,6 @@ const embed = new Discord.MessageEmbed()
 .setDescription("Bots Status")
 client.users.cache.filter(u => u.bot === true).forEach(bot => embed.addField(bot.username, bot.presence.status === "online"?"**🟢ONLINE**":"**🔴OFFLINE**"))
 
-webhook.send(embed).then(msg =>{
-   setInterval(function(){
-       msg.edit(embed)
-    }, 5000)
-})
 
 })
 client.login(process.env.token)
