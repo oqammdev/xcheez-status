32a2b442b3c2913cd0b1e1f0d988507bc61f385a
XcHeeZ <akhgakamirb@gmail.com>
2021-04-22 01:12:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const Discord = require("discord.js")
 let webhook = new Discord.WebhookClient(process.env.id, process.env.secret)
 let client = new Discord.Client()
 
+client.on("ready", async()=>{
+   console.log("ready")
+   
+   let guild = client.guilds.cache.get("792520918312288297")
 
-const xcheezbot = client.users.cache.get("768603762851840012")
-const xcheezutils = client.users.cache.get("773991344813244417")
-const xcheezgens = client.users.cache.get("760785837188448306")
+
+const xcheez0 = guild.members.cache.get("768603762851840012").user
+const xcheez1 = guild.members.cache.get("773991344813244417").user
+const xcheez2 = guild.members.cache.get("760785837188448306")
 
 
-let embed = new Discord.MessageEmbed()
+const embed = new Discord.MessageEmbed()
 .setColor("BLUE")
 .setDescription("Bots Status")
-//.addField("XcHeeZ Bot", xcheezbot.presence.status === "online"?"**🟢ONLINE**":"**🔴OFFLINE**")
-//.addField("XcHeeZ Utilities", xcheezutils.presence.status === "online"?"**🟢ONLINE**":"**🔴OFFLINE**")
-//.addField("XcHeeZ Generators", xcheezgens.presence.status === "online"?"**🟢ONLINE**":"**🔴OFFLINE**")
+.addField("XcHeeZ Bot", xcheez0.presence.status === "online"?"**🟢ONLINE**":"**🔴OFFLINE**")
+.addField("XcHeeZ Utilities", xcheez1.presence.status === "online"?"**🟢ONLINE**":"**🔴OFFLINE**")
+.addField("XcHeeZ Generators", xcheez2.presence.status === "online"?"**🟢ONLINE**":"**🔴OFFLINE**")
 
 webhook.send(embed).then(msg =>{
    setInterval(function(){
@@ -21,4 +26,5 @@ webhook.send(embed).then(msg =>{
     }, 5000)
 })
 
+})
 client.login(process.env.token)
