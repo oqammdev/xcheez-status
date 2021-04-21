1b13f95f3f3a4bc168c1de62523c20dd7406b838
XcHeeZ <akhgakamirb@gmail.com>
2021-04-22 00:54:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv").config()
 const Discord = require("discord.js")
-let webhook = new Discord.WebhookClient(process.env.id, process.env.token)
+let webhook = new Discord.WebhookClient(process.env.id, process.env.secret)
 let client = new Discord.Client()
-client.login(process.env.bottoken)
-let xcheezbot = client.users.resolve("768603762851840012")
-let xcheezutils = client.users.resolve("773991344813244417")
-let xcheezgens = client.users.resolve("760785837188448306")
+client.login(process.env.token)
+let xcheezbot = client.users.cache.get("768603762851840012")
+let xcheezutils = client.users.cache.get("773991344813244417")
+let xcheezgens = client.users.cache.get("760785837188448306")
 
 
 let embed = new Discord.MessageEmbed()
